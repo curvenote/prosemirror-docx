f1e615a286ec3fac82f98449531ee329a9e13469
Daniel Cooke <danielcooke1996@gmail.com>
2025-06-24 20:05:57 +0100

    Export new methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export { DocxSerializerState, DocxSerializer, MarkSerializer, NodeSerializer } from './serializer';
+export {
+  DocxSerializerState,
+  DocxSerializer,
+  MarkSerializer,
+  NodeSerializer,
+  Options,
+} from './serializer';
 export { defaultDocxSerializer, defaultNodes, defaultMarks } from './schema';
-export { writeDocx, createDocFromState } from './utils';
+export { writeDocx, createDocFromState, buildDoc } from './utils';
+export { SectionConfig, SerializationState } from './types';
